a745f5d579e4669c193a876f2eda4f1d2e1efbde
ASHKO <amirshkolnik1@gmail.com>
2025-01-23 09:18:26 +0100

    FizzBuzz_game - add feat: Add FizzBuzz logic to print numbers and words for divisibility
    - Implemented FizzBuzz logic to print Fizz for numbers divisible by 3, Buzz for numbers divisible by 5, and FizzBuzz for numbers divisible by both.
    - Included functionality to print numbers for non-divisible cases.
    - Optimized condition handling with if-elif-else for better readability and performance.
    
    Example output: 1, 2, Fizz, 4, Buzz, ..., FizzBuzz

--- FizzBuzz_game.py
+++ FizzBuzz_game.py
@@ -1,2 +1,9 @@
 for number in range(1, 101):
-    print(number)
+    if number % 5 == 0 and number % 3 == 0:
+        print("FizzBuzz")
+    elif number % 5 == 0:
+        print("Buzz")
+    elif number % 3 == 0:
+        print("Fizz")
+    else:
+        print(number)
