c8d016446846a7db87b46e76aad8dd0794684fe9
ASHKO <amirshkolnik1@gmail.com>
2025-01-25 20:34:17 +0100

    passwor creator - add print password

--- password_creator.py
+++ password_creator.py
@@ -9,4 +9,15 @@ nr_letters = int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 
+password = ""
 
+for letter in range(0, nr_letters):
+    password += random.choice(letters)
+
+for symbol in range(0, nr_symbols):
+    password += random.choice(symbols)
+
+for number in range(0, nr_numbers):
+    password += random.choice(numbers)
+
+print(password)
