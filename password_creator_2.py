b05ebf4b4189c3e5a45c6d9ede38d64e095600a2
ASHKO <amirshkolnik1@gmail.com>
2025-01-26 13:54:59 +0100

    add password and print

--- password_creator_2.py
+++ password_creator_2.py
@@ -3,3 +3,5 @@ import string
 
 length = 12
 characters = string.ascii_letters + string.digits + string.punctuation
+password = ''.join(random.choice(characters) for _ in range(length))
+print("Your random password is:", password)
