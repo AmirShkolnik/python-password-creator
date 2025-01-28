26d495d56397de296c724e9d61416ff6453030df
ASHKO <amirshkolnik1@gmail.com>
2025-01-28 20:23:19 +0100

    chnage fixed length to user imput

--- password_creator_2.py
+++ password_creator_2.py
@@ -1,7 +1,7 @@
 import random
 import string
 
-length = 12
+length = int(input(f"Choose the length of your passwor: \n"))
 characters = string.ascii_letters + string.digits + string.punctuation
 password = ''.join(random.choice(characters) for _ in range(length))
 print("Your random password is:", password)
