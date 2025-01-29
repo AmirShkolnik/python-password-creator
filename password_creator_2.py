9a7a2e7e20a67359fa8afd8d605d6098eeb75e1b
ASHKO <amirshkolnik1@gmail.com>
2025-01-29 18:18:19 +0100

    add a welcome message

--- password_creator_2.py
+++ password_creator_2.py
@@ -1,7 +1,7 @@
 import random
 import string
-
-length = int(input(f"Choose the length of your passwor: \n"))
+print("Welcome to the Password Generator!")
+length = int(input(f"Choose the length of your password: \n"))
 characters = string.ascii_letters + string.digits + string.punctuation
 password = ''.join(random.choice(characters) for _ in range(length))
 print("Your random password is:", password)
